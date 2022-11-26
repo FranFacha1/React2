ddff6202ae93dabbfb199b8c20f9a4e94df189c7
francisco bellotti <franciscobellotti2002@gmail.com>
2022-11-25 21:45:58 -0300

    Creando NavBar

---
+++ src/components/NavBar/Carrito/Carrito.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Carrito.css"
+
+function Carrito() {
+  const ftoCarrito = "https://cdn-icons-png.flaticon.com/512/834/834526.png" 
+  return (
+    <div className='div-carrito'>
+      <img className='carrito' src={ftoCarrito}></img>
+    </div>
+  )
+}
+
+export default Carrito
